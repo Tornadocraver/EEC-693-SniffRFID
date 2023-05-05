742e19d5ba7ac0cdc65b429ae3f7d70a0d4f8179
Micah Vertal <micahmail9@gmail.com>
2023-05-05 01:16:04 -0400

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SniffRFID;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+#if ANDROID
+using SniffRFID.Platforms.Android;
+#endif
+
+namespace SniffRFID;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+// ** CODE BELOW DID NOT WORK, BUT LEFT IN FOR FUTURE REFERENCE ** \\
+//
+//#if ANDROID
+//		builder.Services.AddSingleton<MainPage>();
+//		builder.Services.AddTransient<IService, EmulatorService>();
+//#endif
+
+		return builder.Build();
+	}
+}
